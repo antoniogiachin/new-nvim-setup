365c41684dd5cb84deaa963a8058e9450176f60f
antoniogiachin <giachinantonio@outlook.com>
2024-01-15 08:18:42 +0100

    change find files shortcut

--- lua/standard/plugins/telescope.lua
+++ lua/standard/plugins/telescope.lua
@@ -3,14 +3,12 @@ return {
     "nvim-telescope/telescope-ui-select.nvim",
   },
   {
-    'nvim-telescope/telescope-fzf-native.nvim',
-    build =
-    'cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release && cmake --install build --prefix build',
-  },
-  {
     "nvim-telescope/telescope.nvim",
     tag = "0.1.5",
-    dependencies = { "nvim-lua/plenary.nvim" },
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      { 'nvim-telescope/telescope-fzf-native.nvim', build = 'make' }
+    },
     config = function()
       require("telescope").setup({
         extensions = {
@@ -32,7 +30,7 @@ return {
         }
       })
       local builtin = require("telescope.builtin")
-      vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
+      vim.keymap.set('n', '<leader><leader>', builtin.find_files, {})
       vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
       vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
       vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
